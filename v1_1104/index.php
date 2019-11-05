18f7fbe106a6d4186fa74d0e35ca7c543200d6d4
Zoe <50683846+ZoeShe@users.noreply.github.com>
2019-11-04 20:02:36 -0800

    updated php layout

---
+++ v1_1104/index.php
@@ -0,0 +1,10 @@
+<?php include("header.html"); ?>
+
+<h1>Welcome to Zoe's Little World!</h1>
+<nav>
+    <div class="category-container website">
+        <a href="website.html" class="open-popup">Website</a>
+    </div>
+</nav>
+
+<?php include("footer.html"); ?>
