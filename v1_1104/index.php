ceee4410ef6ff93c293058a6ad334aa0a7bb11f0
Zoe <50683846+ZoeShe@users.noreply.github.com>
2019-11-04 22:36:37 -0800

    index layout success

--- v1_1104/index.php
+++ v1_1104/index.php
@@ -1,10 +1,25 @@
 <?php include("header.html"); ?>
 
 <h1>Welcome to Zoe's Little World!</h1>
+
 <nav>
-    <div class="category-container website">
-        <a href="website.html" class="open-popup">Website</a>
-    </div>
+    <div class="nav-item"><a href="#website">Website</a></div>
+    <div class="nav-item"><a href="#website">Website</a></div>
+    <div class="nav-item"><a href="#website">Website</a></div>
+    <div class="nav-item"><a href="#website">Website</a></div>
+    <div class="nav-item"><a href="#website">Website</a></div>
+    <div class="nav-item"><a href="#website">Website</a></div>
 </nav>
+
+
+
+
+
+<div id="website" class="popup">
+<div class="popup_container">
+      <?php include("website.html"); ?>
+    </div>
+</div>
+
 
 <?php include("footer.html"); ?>
