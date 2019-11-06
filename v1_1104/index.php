c8f0fa61ec576b3fad72e8acc83b44dd971f62de
Zoe <50683846+ZoeShe@users.noreply.github.com>
2019-11-05 16:34:36 -0800

    built category pages

--- v1_1104/index.php
+++ v1_1104/index.php
@@ -3,23 +3,56 @@
 <h1>Welcome to Zoe's Little World!</h1>
 
 <nav>
-    <div class="nav-item"><a href="#website">Website</a></div>
-    <div class="nav-item"><a href="#website">Website</a></div>
-    <div class="nav-item"><a href="#website">Website</a></div>
-    <div class="nav-item"><a href="#website">Website</a></div>
-    <div class="nav-item"><a href="#website">Website</a></div>
-    <div class="nav-item"><a href="#website">Website</a></div>
+    <div class="nav-item"><a href="#motion_graphics">Motion Graphics</a></div>
+    <div class="nav-item"><a href="#website_productions">Website Productions</a></div>
+    <div class="nav-item"><a href="#strategy_presentations">Strategy Presentations</a></div>
+    <div class="nav-item"><a href="#e-commerce_contents">E-commerce Contents</a></div>
+    <div class="nav-item"><a href="#exhibition_booths">Exhibition Booths</a></div>
+    <div class="nav-item"><a href="#3d_modeling">3D Modeling</a></div>
+    <div class="nav-item"><a href="#apparel_manufacturing">Apparel Manufacturing</a></div>
 </nav>
 
 
-
-
-
-<div id="website" class="popup">
-<div class="popup_container">
-      <?php include("website.html"); ?>
+<div class="popup" id="motion_graphics">
+    <div class="popup_container">
+        <?php include("motion_graphics.html"); ?>
     </div>
 </div>
 
+<div class="popup" id="website_productions">
+    <div class="popup_container">
+        <?php include("website_productions.html"); ?>
+    </div>
+</div>
+
+<div class="popup" id="strategy_presentations">
+    <div class="popup_container">
+        <?php include("strategy_presentations.html"); ?>
+    </div>
+</div>
+
+<div class="popup" id="e-commerce_contents">
+    <div class="popup_container">
+        <?php include("e-commerce_contents.html"); ?>
+    </div>
+</div>
+
+<div class="popup" id="exhibition_booths">
+    <div class="popup_container">
+        <?php include("exhibition_booths.html"); ?>
+    </div>
+</div>
+
+<div class="popup" id="3d_modeling">
+    <div class="popup_container">
+        <?php include("3d_modeling.html"); ?>
+    </div>
+</div>
+
+<div class="popup" id="apparel_manufacturing">
+    <div class="popup_container">
+        <?php include("apparel_manufacturing.html"); ?>
+    </div>
+</div>
 
 <?php include("footer.html"); ?>
